8eeb539f75756cfb280a42a174dee71e2286564c
William Morriss <wjmelements@gmail.com>
2025-09-16 17:58:38 -0500

    document the decay function

--- src/Dutch.sol
+++ src/Dutch.sol
@@ -2,18 +2,19 @@
 pragma solidity ^0.8.30;
 
 import {exp2} from "@prb-math/Common.sol";
-import {UD60x18} from "@prb-math/UD60x18.sol";
+import {UD60x18, uUNIT} from "@prb-math/UD60x18.sol";
 
 library Dutch {
-    uint256 private constant INTERVAL = 7 days;
-    uint256 private constant PRECISION = 10 ** 18;
-    // decays smoothly by 1/4 every week
-    // P * (4 ** -(delay / interval))
-    // P / (4 ** (delay / interval))
+    uint256 private constant HALVING_INTERVAL = 3.5 days;
 
+    /**
+     * @notice Exponential decay by 1/4 per week
+     * @param startPrice The initial price at elapsed = 0
+     * @param elapsed The amount of time since the startPrice
+     */
     function decay(uint256 startPrice, uint256 elapsed) internal pure returns (uint256 price) {
         UD60x18 coefficient = UD60x18.wrap(startPrice);
-        UD60x18 decayFactor = UD60x18.wrap(elapsed * PRECISION * 2 / INTERVAL).exp2();
+        UD60x18 decayFactor = UD60x18.wrap(elapsed * uUNIT / HALVING_INTERVAL).exp2();
 
         return coefficient.div(decayFactor).unwrap();
     }
