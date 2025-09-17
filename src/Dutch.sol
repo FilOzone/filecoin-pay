fe8671e636b89ff70593445f79390e8c2790d8a8
William Morriss <wjmelements@gmail.com>
2025-09-17 18:05:06 -0500

    Dutch.MAX_DECAY

--- src/Dutch.sol
+++ src/Dutch.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 pragma solidity ^0.8.30;
 
-import {UD60x18, uUNIT} from "@prb-math/UD60x18.sol";
+import {UD60x18, uEXP2_MAX_INPUT, uUNIT} from "@prb-math/UD60x18.sol";
 
 /**
  * @dev Recurring dutch auction
@@ -9,6 +9,7 @@ import {UD60x18, uUNIT} from "@prb-math/UD60x18.sol";
 library Dutch {
     uint256 public constant RESET_FACTOR = 4;
     uint256 public constant HALVING_INTERVAL = 3.5 days;
+    uint256 public constant MAX_DECAY = uEXP2_MAX_INPUT * HALVING_INTERVAL / uUNIT;
 
     /**
      * @notice Exponential decay by 1/4 per week
@@ -16,6 +17,9 @@ library Dutch {
      * @param elapsed The amount of time since the startPrice
      */
     function decay(uint256 startPrice, uint256 elapsed) internal pure returns (uint256 price) {
+        if (elapsed > MAX_DECAY) {
+            return 0;
+        }
         UD60x18 coefficient = UD60x18.wrap(startPrice);
         UD60x18 decayFactor = UD60x18.wrap(elapsed * uUNIT / HALVING_INTERVAL).exp2();
 
--- test/Dutch.t.sol
+++ test/Dutch.t.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.30;
 
 import {Test} from "forge-std/Test.sol";
+import /*{Dutch} from*/ "../src/Dutch.sol";
+import {uUNIT} from "@prb-math/UD60x18.sol";
 
-import {Dutch} from "../src/Dutch.sol";
+contract ExternalDutch {
+    using Dutch for uint256;
+
+    function dutch(uint256 startPrice, uint256 elapsed) external pure returns (uint256) {
+        return startPrice.decay(elapsed);
+    }
+}
 
 contract DutchTest is Test {
     using Dutch for uint256;
@@ -24,5 +32,36 @@ contract DutchTest is Test {
         checkExactDecay(9 ether);
         checkExactDecay(11 ether);
         checkExactDecay(13 ether);
+        checkExactDecay(1300000 ether);
+    }
+
+    function testMaxDecayU256() public pure {
+        uint256 maxPrice = 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;
+
+        assertEq(maxPrice.decay(0), maxPrice);
+        assertEq(maxPrice.decay(10000000), 12852371374314799914919560702529050018701224735495877087613516410500);
+        assertEq(maxPrice.decay(50000000), 1950746206018947071427216775);
+        assertEq(maxPrice.decay(58060000), 18480601319969968529);
+        assertEq(maxPrice.decay(Dutch.MAX_DECAY - 1), 18446828639436756833);
+        assertEq(maxPrice.decay(Dutch.MAX_DECAY), 18446786356524694827);
+        assertEq(maxPrice.decay(Dutch.MAX_DECAY + 1), 0);
+    }
+
+    function testMaxDecayFIL() public pure {
+        uint256 maxPrice = 2 * 10 ** 27; // max FIL supply
+
+        assertEq(maxPrice.decay(0), maxPrice);
+        assertEq(maxPrice.decay(90 days), 36329437917604310558);
+        assertEq(maxPrice.decay(10000000), 221990491042506894);
+        assertEq(maxPrice.decay(20000000), 24639889);
+        assertEq(maxPrice.decay(23000000), 25423);
+        assertEq(maxPrice.decay(26000000), 26);
+        assertEq(maxPrice.decay(26500000), 8);
+        assertEq(maxPrice.decay(27000000), 2);
+        assertEq(maxPrice.decay(27425278), 1);
+        assertEq(maxPrice.decay(27425279), 0);
+        assertEq(maxPrice.decay(Dutch.MAX_DECAY - 1), 0);
+        assertEq(maxPrice.decay(Dutch.MAX_DECAY), 0);
+        assertEq(maxPrice.decay(Dutch.MAX_DECAY + 1), 0);
     }
 }
